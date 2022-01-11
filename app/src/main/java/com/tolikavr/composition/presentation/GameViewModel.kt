5679bea70f16e1f0009b5f50e899f269380937e7
tolikavr <anatolii13@mail.ru>
2022-01-11 16:58:50 +0300

    GameFragment.kt implementation

--- app/src/main/java/com/tolikavr/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/tolikavr/composition/presentation/GameViewModel.kt
@@ -28,7 +28,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
   private var timer: CountDownTimer? = null
 
   private val _formattedTime = MutableLiveData<String>()
-  private val formattedTime: LiveData<String>
+  val formattedTime: LiveData<String>
     get() = _formattedTime
 
   private val _question = MutableLiveData<Question>()
@@ -66,6 +66,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     getGameSettings(level)
     startTimer()
     generateQuestion()
+    updateProgress()
   }
 
   fun chooseAnswer(number: Int) {
@@ -87,6 +88,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
   }
 
   private fun calculatePercentOfRightAnswers(): Int {
+    if (countOfQuestions == 0) return 0
     return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
   }
 
